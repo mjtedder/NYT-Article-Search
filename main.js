b2251fe3435183e2453404f67dee1a0975f332fc
mjtedder <mjtedder@gmail.com>
2018-12-21 11:56:35 -0700

    Changed ajax call to return articles

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-$(document).ready(function () {
 
-var url = "http://api.nytimes.com/svc/archive/v1/2016/11.json?api-key=217063ed3d2b47469db37df8ee39f512";
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
 
-    $('#search').on('click', function () {
-        alert('button clicked');
-    })
 
-})
+
+
+
+
+  $('#clearAll').on('click', function () {
+    // Creating a div to hold the movie
+    
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "217063ed3d2b47469db37df8ee39f512",
+    'q': "nintendo",
+    'hl': "true"
+  });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function (result) {
+    var docs = result.response.docs
+    console.log(docs)
+    console.log(result);
+  }).fail(function (err) {
+    throw err;
+  });
+    var infoDiv = $("<div class='info'>");
+    var pOne = $("<p>").text("Rating: ");
+    $(infoDiv).append(pOne)
+    $('#well-section').prepend(infoDiv);
+    console.log(infoDiv);
+  })
