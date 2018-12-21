ce62d2d1cf3b063d412cff5c55eea9d3bc2e1190
mjtedder <mjtedder@gmail.com>
2018-12-21 14:45:17 -0700

    Clean up code, variables, methods, event listeners. TODO: allow user input trigger api call

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
+// SETUP VARIABLES ==============================================================
+
+// Variable to store API Key
+var authkey = '217063ed3d2b47469db37df8ee39f512';
+
+// These variables will store user input on our page
+var searchTerm = "";
+var numResults = 0;
+var startYear = 0;
+var endYear = 0;
+
+// This url will append to searchTerm once user makes an api call
+var queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authkey + "&q=";
 
 
+// METHODS =====================================================================
 
 
-
-
-
-
-  $('#clearAll').on('click', function () {
-    // Creating a div to hold the movie
-    
+function displayInfo() {
+  // Creating a div to hold the movie
   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
   url += '?' + $.param({
     'api-key': "217063ed3d2b47469db37df8ee39f512",
@@ -18,16 +27,36 @@
   $.ajax({
     url: url,
     method: 'GET',
-  }).done(function (result) {
-    var docs = result.response.docs
-    console.log(docs)
+  }).then(function (result) {
+    // Storing the headline
+    var headline = result.response.docs[0].headline.main;
+    console.log(headline)
     console.log(result);
+     // Creating a div to hold the data
+     var resultsDiv = $("<div class='results'>");
+     // Creating an element to have the headline displayed
+     var pOne = $("<p>").text("Headline: " + headline);
+     // Displaying the results
+     resultsDiv.append(pOne);
+    // Adding to browser
+    $('#well-section').prepend(resultsDiv);
   }).fail(function (err) {
     throw err;
   });
-    var infoDiv = $("<div class='info'>");
-    var pOne = $("<p>").text("Rating: ");
-    $(infoDiv).append(pOne)
-    $('#well-section').prepend(infoDiv);
-    console.log(infoDiv);
-  })
+}
+
+// This function handles events where a movie button is clicked
+$("#search").on("click", function(event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var term = $("#searchTermInput2").val().trim();
+
+  // Adding movie from the textbox to our array
+  movies.push(movie);
+
+  // Calling renderButtons which handles the processing of our movie array
+  renderButtons();
+});
+$('#clearAll').on('click', function () {
+  displayInfo();
+});
